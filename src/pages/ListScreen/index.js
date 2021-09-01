4e131bda8165b024c3920dfff5432f8ded3419e3
Lucas <dexterfail@hotmail.com>
2021-09-01 09:04:07 -0300

    tela de listagem

--- src/pages/ListScreen/index.js
+++ src/pages/ListScreen/index.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {Container, Texto, Botao} from './styles';
+import { useSelector } from "react-redux";
+import NoteItem from "../../components/NoteItem";
+
+import {
+  Container,
+  AddButton,
+  AddButtonImage,
+  NotesList,
+  NoNotes,
+  NoNotesImage,
+  NoNotesText
+} from './styles';
 
 export default () => {
   const navigation = useNavigation();
+  const list = useSelector(state => state.notes.list);
+
+  useLayoutEffect(()=> {
+    console.log(list)
+     navigation.setOptions({
+       title:"Suas Notas",
+       headerRight: () => (
+         <AddButton underlayColor="transparent" onPress={()=>navigation.navigate("EditNote")}>
+           <AddButtonImage source={require("../../assets/more.png")}/>
+         </AddButton>
+       )
+     })
+  },[])
+
+  const handleNotePress = (index) => {
+    navigation.navigate("EditNote", {
+      key:index
+    })
+  }
+
   return (
     <Container>
-      <Texto>Tela de listar</Texto>
-      <Botao title="Ir para EDITAR" onPress={()=>navigation.navigate("EditNote")}/>
+      {list.length > 0 &&
+        <NotesList
+          data={list}
+          renderItem={({item,index})=>(
+            <NoteItem
+              data={item}
+              index={index}
+              onPress={handleNotePress}
+            />
+          )}
+          keyExtractor={(item,index)=>index.toString()}
+        />
+      }
+      {list.length == 0 && 
+        <NoNotes>
+          <NoNotesImage source={require("../../assets/note.png")}/>
+          <NoNotesText>Nenhuma Anotação</NoNotesText>
+        </NoNotes>
+      }
     </Container>
   );
 };
